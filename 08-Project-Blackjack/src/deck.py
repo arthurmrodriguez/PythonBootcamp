9c4067ab17f5f2c7a181bc90dbc100a09fedb334
arthurmrodriguez <amrn2404@gmail.com>
2019-01-31 23:47:35 +0100

    Update class Deck to be ready for game

--- 08-Project-Blackjack/src/deck.py
+++ 08-Project-Blackjack/src/deck.py
@@ -25,6 +25,7 @@ class Deck():
 
     # Method for adding a new card only
     def add_card(self,card):
+      
         if card not in self.cards:
             self.cards.append(card)
         else:
@@ -36,7 +37,8 @@ class Deck():
 
     # Dunder method str
     def __str__(self):
+      
         cards = ''
-        for card in self.cards:
-            cards += str(card)+'\n'
+        for item in self.cards:
+            cards += str(item)+'\n'
         return cards
